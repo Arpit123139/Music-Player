7e8e1780017c6dd2ea110607f25578cef2ec742a
Arpit123139 <arpitgajya2001@gmail.com>
2022-10-13 00:40:32 +0530

    Setting Up The scroll bar icon and the scroll bar in Recycler View

--- app/src/main/java/com/example/spotify/MainActivity.kt
+++ app/src/main/java/com/example/spotify/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.spotify
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Color
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
@@ -11,10 +12,12 @@ import android.view.MenuItem
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.spotify.databinding.ActivityMainBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import java.io.File
 import kotlin.system.exitProcess
 
@@ -71,6 +74,7 @@ class MainActivity : AppCompatActivity() {
 
 
 
+        /********************************************8SETTING UP A ALERT DIALOG BOX FOR EXIT OPTION*************************/
         binding.navView.setNavigationItemSelectedListener{
             when(it.itemId){
                 R.id.navFeedback->Toast.makeText(this,"Feedback Clicked",Toast.LENGTH_SHORT).show()
@@ -79,7 +83,27 @@ class MainActivity : AppCompatActivity() {
 
                 R.id.navSettings->Toast.makeText(this,"Settings Clicked",Toast.LENGTH_SHORT).show()
 
-                R.id.navExit-> exitProcess(1)                  // This indicate that we want to close the application Knowingly
+                R.id.navExit-> {
+                    val builder=MaterialAlertDialogBuilder(this)
+                    builder.setTitle("Exit")
+                        .setMessage("Do you want to close App")
+                        .setPositiveButton("Yes"){_,_->
+                            if(PlayerActivity.musicService!=null){                  /*************This Condition is there because when there is No Foreground Service running that is initially withouth clicking any song then no need to stop foreground service **/
+                                PlayerActivity.musicService!!.stopForeground(true)
+                                PlayerActivity.musicService!!.mediaPlayer!!.release()            // The resources that are occupied by the media Player must be released
+                                PlayerActivity.musicService=null;
+                            }
+                            exitProcess(1)
+                        }
+                        .setNegativeButton("No"){dialog,_->
+                            dialog.dismiss()
+                        }
+                    val customDialog=builder.create()
+                    customDialog.show()
+                    customDialog.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(Color.RED)
+                    customDialog.getButton(AlertDialog.BUTTON_NEGATIVE).setTextColor(Color.RED)
+
+                }                 // This indicate that we want to close the application Knowingly
             }
             // this is a method to write a return statement
             true
